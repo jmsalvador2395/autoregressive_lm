faf05373de35df90fa4eda144eb0c58e471ee5b9
john <jmsalvador2395@gmail.com>
2022-11-29 03:36:08 -0600

    moved argparser content into utils. also added data_utils for data related functions

---
+++ src/data_utils.py
@@ -0,0 +1,121 @@
+from pathlib import Path
+import argparse
+import datasets
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+import torchtext
+import os
+import pickle
+
+# relative imports
+from . import utils
+
+def get_data(ds_name='pg19', ds_dir='datasets', batch_size=16):
+
+	# build full directory path
+	root_dir = utils.get_project_root()
+	target_dir = root_dir + '/' + ds_dir
+
+	# create path for dataset if it doesn't exist
+	path = Path(target_dir).mkdir(parents=True, exist_ok=True)
+	datasets.config.DOWNLOADED_DATASETS_PATH = path
+	
+	# download or read in dataset
+	data = load_dataset(
+		ds_name,
+		cache_dir = target_dir
+	).with_format('torch')
+
+	# create dictionary of dataloaders depending on the splits
+	splits=list(data.keys())
+	dataloaders={split:DataLoader(data[split], batch_size=batch_size) for split in splits}
+
+	return data, dataloaders
+
+def tokenize_data(data, ds_name='pg19', save_dir=None):
+	"""
+	tokenizes a given dataset
+
+	input:
+		data : the huggingface dataset
+		ds_name : name of the dataset
+		save_dir : the save directory for the dataset (default: <project root>/datasets/tokenized/)
+	
+	output:
+		the tokenized dataset
+	"""
+	# use default directory if none is provided
+	if save_dir is None:
+		save_dir = utils.get_project_root() + f'/datasets/tokenized/{ds_name}'
+
+	# check if the tokenized dataset exists and load. otherwise proceed with tokenization
+	if os.path.exists(save_dir):
+		tokenized_data = datasets.load_from_disk(save_dir)
+		return tokenized_data
+	
+	# make sure path exists
+	path = Path(save_dir).mkdir(parents=True, exist_ok=True)
+
+	# tokenize the dataset
+	# source: https://towardsdatascience.com/language-modeling-with-lstms-in-pytorch-381a26badcbf
+	print('creating tokenized dataset (this only needs to happen once per dataset)...')
+	tokenizer = torchtext.data.utils.get_tokenizer('basic_english')
+	tokenize = lambda sample, tokenizer: {'tokens' : tokenizer(sample['text'])}
+	tokenized_data = data.map(
+		tokenize,
+		remove_columns=['text'],
+		fn_kwargs={'tokenizer' : tokenizer}
+	)
+
+	# save the dataset
+	tokenized_data.save_to_disk(save_dir)
+	print(f'tokenized dataset saved to {save_dir + ds_name}')
+
+	return tokenized_data
+
+def build_vocab(data, ds_name='pg19', token_ds_dir=None, vocab_dir=None, min_freq=3):
+	"""
+	builds the vocabulary for the dataset
+	source: https://towardsdatascience.com/language-modeling-with-lstms-in-pytorch-381a26badcbf
+
+	input:
+		data : the huggingface dataset
+		ds_name : name of the dataset
+		save_dir : the save directory for the dataset (default: <project root>/datasets/tokenized/)
+	
+	output:
+		the dataset vocabulary
+	"""
+	if vocab_dir is None:
+		vocab_dir = utils.get_project_root() + f'/datasets/tokenized/{ds_name}'
+		vocab_path = vocab_dir + '/vocab.pkl'
+	
+	# if vocab already exists, load and return
+	if os.path.isfile(vocab_path):
+		with open(vocab_path, 'rb') as f:
+			vocab = pickle.load(f)
+		import pdb
+		pdb.set_trace()
+		return vocab
+
+	# get the tokenized dataset
+	tokenized_data = tokenize_data(data, ds_name=ds_name, save_dir=token_ds_dir)
+
+	# build vocab
+	print('building vocabulary (this only needs to happen once per dataset)...')
+	vocab = torchtext.vocab.build_vocab_from_iterator(
+		tokenized_data['train']['tokens'],
+		min_freq=min_freq
+	)
+	vocab.insert_token('<unk>', 0)
+	vocab.insert_token('<eos>', 1)
+	vocab.set_default_index(vocab['<unk>'])
+	print('done')
+
+	# save the vocab
+	print(f'saving the vocab to {vocab_path}')
+	with open(vocab_path, 'wb') as f:
+		pickle.dump(vocab, f)
+	print('done')
+
+	return vocab
